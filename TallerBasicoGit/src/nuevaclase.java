11b9b7ee3dd003a59c3a992e89cc7bad6737c28b
jmolinet <conatao@hotmail.com>
2014-04-07 17:17:20 -0300

    nuevo cambio web

--- TallerBasicoGit/src/nuevaclase.java
+++ TallerBasicoGit/src/nuevaclase.java
@@ -3,7 +3,7 @@ public class nuevaclase {
 	public static void main(String[] args){
 		
 		System.out.println("jaja");
-		System.out.println("linea agregada desde la web");
+		System.out.println("linea agregada desde la web, modifica en web");
 	}
 	
 }
