b936b8affd45a746716cdf8e6415a3c06a87fbd5
JOHANN <JOHANN@10.3.14.92>
2014-04-07 17:19:59 -0300

    NUEVO COMMIT

--- TallerBasicoGit/src/nuevaclase.java
+++ TallerBasicoGit/src/nuevaclase.java
@@ -3,7 +3,8 @@ public class nuevaclase {
 	public static void main(String[] args){
 		
 		System.out.println("jaja");
-		System.out.println("linea agregada desde la web");
+		System.out.println("linea agregada desde la web,jajjjskggsUGJAG");
+		
 	}
 	
 }
